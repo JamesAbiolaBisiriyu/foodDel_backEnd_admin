e2404fd0dbee732b10b4da3cea8aca1070cc6a07
james <emperor2g@gmail.com>
2025-02-12 12:00:24 +0100

    5:06

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,13 +7,13 @@ const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className="sidebar-options">
-        <NavLink to='/adds' className="sidebar-option" end>
+        <NavLink to='/add' className="sidebar-option" end>
           <img src={assets.add_icon} alt="" />
           <p>Add Items</p>
         </NavLink>
       </div>
       <div className="sidebar-options">
-        <NavLink to='/lists' className="sidebar-option" end>
+        <NavLink to='/list' className="sidebar-option" end>
           <img src={assets.order_icon} alt="" />
           <p>List Items</p>
         </NavLink>
